77d9bcb5f74012247dfd780e3a9534816d23a0b4
anaktjkt <anaktjkt@gmail.com>
2025-01-13 19:46:47 +0700

    feature(navbar): add responsive layout, add menu toggle, and styling navbar

---
+++ sunnyside-agency-landing-page-main/script.js
@@ -0,0 +1,13 @@
+const navbarNav = document.querySelector('.navbarNav');
+// ketika hamburger menu dikilik
+document.querySelector('hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+}
+// klik diluar sidebar untuk menghilangkan navbar
+const hamburger = document.querySelector('.hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if( !hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
+}) 

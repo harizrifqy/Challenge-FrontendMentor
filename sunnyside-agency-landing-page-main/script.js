a1815258079e6b8110e1c3083cc6310e0c2190a4
anaktjkt <anaktjkt@gmail.com>
2025-01-13 21:34:57 +0700

    feature(header): add header-img responsive, fix class typing errors, and update style navbar &header

--- sunnyside-agency-landing-page-main/script.js
+++ sunnyside-agency-landing-page-main/script.js
@@ -1,13 +1,13 @@
-const navbarNav = document.querySelector('.navbarNav');
+const navLinks = document.querySelector('.nav-links');
 // ketika hamburger menu dikilik
-document.querySelector('hamburger-menu').onclick = () => {
-    navbarNav.classList.toggle('active');
+document.querySelector('.hamburger-menu').onclick = () => {
+    navLinks.classList.toggle('active');
 }
 // klik diluar sidebar untuk menghilangkan navbar
 const hamburger = document.querySelector('.hamburger-menu');
 
 document.addEventListener('click', function(e) {
-    if( !hamburger.contains(e.target) && !navbarNav.contains(e.target)){
-        navbarNav.classList.remove('active');
+    if( !hamburger.contains(e.target) && !navLinks.contains(e.target)){
+        navLinks.classList.remove('active');
     }
 }) 
